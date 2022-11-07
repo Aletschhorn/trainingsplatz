11a06ced67bf509c1bb54f3b81adea8a59a018d4
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2022-11-07 21:02:53 +0100

    Replace array() by []

--- Configuration/TCA/tx_trainingsplatz_domain_model_training.php
+++ Configuration/TCA/tx_trainingsplatz_domain_model_training.php
@@ -1,6 +1,6 @@
 <?php
-return array(
-	'ctrl' => array(
+return [
+	'ctrl' => [
 		'title'	=> 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training',
 		'label' => 'title',
 		'tstamp' => 'tstamp',
@@ -10,322 +10,322 @@ return array(
 		'versioningWS' => TRUE,
 		'default_sortby' => 'ORDER BY training_date DESC',
 		'delete' => 'deleted',
-		'enablecolumns' => array(
+		'enablecolumns' => [
 			'disabled' => 'hidden',
 			'starttime' => 'starttime',
 			'endtime' => 'endtime',
-		),
+		],
 		'searchFields' => 'author,leader,creation_date,last_change,training_date,guided,title,description,start_text,start_coordinates,duration,distance,speed,route,picture,cancelled,intensity,sport,map,',
 		'iconfile' => 'EXT:trainingsplatz/Resources/Public/Icons/tx_trainingsplatz_domain_model_training.png'
-	),
-	'interface' => array(
+	],
+	'interface' => [
 		'showRecordFieldList' => 'title, hidden, public, closed, author, creation_date, last_change, training_date, guided, leader, duration, distance, speed, cancelled, intensity, sport, map, map_center, map_zoom, map_type, infomail, notification',
-	),
-	'types' => array(
-		'1' => array('showitem' => '--palette--;;displayOptions, --palette--;;creationChange, --palette--;;notifications, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_content, author, --palette--;;sportcoach, --palette--;;dateTitle, --palette--;;sportIntensity, start_text, duration, distance, speed, description, picture, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_map, start_option, start_coordinates, route, map, --palette--;;drawMap, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_series, series, series_start, series_end, series_period, series_number, series_weekday, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'),
-	),
-	'palettes' => array(
-		'1' => array('showitem' => ''),
-		'displayOptions' => array('showitem' => 'hidden, public, cancelled, closed'),
-		'creationChange' => array('showitem' => 'creation_date, last_change'),
-		'notifications' => array('showitem' => 'infomail, notification'),
-		'dateTitle' => array('showitem' => 'training_date, title'),
-		'sportIntensity' => array('showitem' => 'sport, intensity'),
-		'sportcoach' => array('showitem' => 'guided, leader'),
-		'drawMap' => array('showitem' => 'map_center, map_zoom, map_type'),
-	),
-	'columns' => array(
+	],
+	'types' => [
+		'1' => ['showitem' => '--palette--;;displayOptions, --palette--;;creationChange, --palette--;;notifications, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_content, author, --palette--;;sportcoach, --palette--;;dateTitle, --palette--;;sportIntensity, start_text, duration, distance, speed, description, picture, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_map, start_option, start_coordinates, route, map, --palette--;;drawMap, --div--;LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.tab_series, series, series_start, series_end, series_period, series_number, series_weekday, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'],
+	],
+	'palettes' => [
+		'1' => ['showitem' => ''],
+		'displayOptions' => ['showitem' => 'hidden, public, cancelled, closed'],
+		'creationChange' => ['showitem' => 'creation_date, last_change'],
+		'notifications' => ['showitem' => 'infomail, notification'],
+		'dateTitle' => ['showitem' => 'training_date, title'],
+		'sportIntensity' => ['showitem' => 'sport, intensity'],
+		'sportcoach' =>['showitem' => 'guided, leader'],
+		'drawMap' => ['showitem' => 'map_center, map_zoom, map_type'],
+	],
+	'columns' => [
 	
-		't3ver_label' => array(
+		't3ver_label' => [
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'max' => 255,
-			)
-		),
+			]
+		],
 	
-		'hidden' => array(
+		'hidden' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
-			),
-		),
-		'starttime' => array(
+			]
+		],
+		'starttime' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 13,
 				'max' => 20,
 				'eval' => 'datetime',
 				'checkbox' => 0,
 				'default' => 0,
-				'range' => array(
+				'range' => [
 					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-				),
-			),
-		),
-		'endtime' => array(
+				],
+			]
+		],
+		'endtime' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 13,
 				'max' => 20,
 				'eval' => 'datetime',
 				'checkbox' => 0,
 				'default' => 0,
-				'range' => array(
+				'range' => [
 					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-				),
-			),
-		),
+				]
+			]
+		],
 
-		'title' => array(
+		'title' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.title',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			),
-		),
-		'description' => array(
+			]
+		],
+		'description' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.description',
-			'config' => array(
+			'config' => [
 				'type' => 'text',
 				'cols' => 40,
 				'rows' => 15,
-			),
-		),
-		'author' => array(
+			]
+		],
+		'author' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.author',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'foreign_table' => 'fe_users',
 				'foreign_table_where' => 'ORDER BY username',
-				'items' => array (
-					array ('(nobody)', 0)
-				)
-			),
-		),
-		'leader' => array(
+				'items' => [
+					['(nobody)', 0]
+				]
+			]
+		],
+		'leader' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.leader',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'foreign_table' => 'fe_users',
 				'foreign_table_where' => 'ORDER BY username',
-				'items' => array (
-					array ('(nobody)', 0)
-				)
-			),
-		),
-		'creation_date' => array(
+				'items' => [
+					['(nobody)', 0]
+				]
+			]
+		],
+		'creation_date' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.creation_date',
-			'config' => array(
+			'config' => [
 				'dbType' => 'datetime',
 				'type' => 'input',
 				'renderType' => 'inputDateTime',
 				'eval' => 'datetime',
 				'default' => '0000-00-00 00:00:00'
-			),
-		),
-		'last_change' => array(
+			],
+		],
+		'last_change' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.last_change',
-			'config' => array(
+			'config' => [
 				'dbType' => 'datetime',
 				'type' => 'input',
 				'renderType' => 'inputDateTime',
 				'eval' => 'datetime',
 				'default' => '0000-00-00 00:00:00'
-			),
-		),
-		'training_date' => array(
+			]
+		],
+		'training_date' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.training_date',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'dbType' => 'date',
 				'renderType' => 'inputDateTime',
 				'eval' => 'date,int',
-			),
-		),
-		'guided' => array(
+			]
+		],
+		'guided' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.guided',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
-		'start_text' => array(
+			]
+		],
+		'start_text' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.start_text',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			),
-		),
-		'start_option' => array(
+			],
+		],
+		'start_option' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.start_option',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 5,
 				'eval' => 'int'
-			),
-		),
-		'start_coordinates' => array(
+			],
+		],
+		'start_coordinates' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.start_coordinates',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			),
-		),
-		'duration' => array(
+			],
+		],
+		'duration' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.duration',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			)
-		),
-		'distance' => array(
+			]
+		],
+		'distance' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.distance',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			)
-		),
-		'speed' => array(
+			]
+		],
+		'speed' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.speed',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			)
-		),
-		'route' => array(
+			]
+		],
+		'route' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.route',
-			'config' => array(
+			'config' => [
 				'type' => 'text',
 				'cols' => 40,
 				'rows' => 15,
 				'eval' => 'trim'
-			)
-		),
-		'picture' => array(
+			]
+		],
+		'picture' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.picture',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'eval' => 'trim'
-			),
-		),
-		'cancelled' => array(
+			],
+		],
+		'cancelled' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.cancelled',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
-		'intensity' => array(
+			]
+		],
+		'intensity' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.intensity',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'foreign_table' => 'tx_trainingsplatz_domain_model_intensity',
-			),
-		),
-		'sport' => array(
+			],
+		],
+		'sport' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.sport',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'foreign_table' => 'tx_trainingsplatz_domain_model_sport',
-			),
-		),
-		'map' => array(
+			],
+		],
+		'map' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.map',
-			'config' => array(
+			'config' => [
 				'type' => 'inline',
 				'foreign_table' => 'tx_trainingsplatz_domain_model_map',
 				'minitems' => 0,
 				'maxitems' => 1,
-				'appearance' => array(
+				'appearance' => [
 					'collapseAll' => 0,
 					'levelLinksPosition' => 'top',
 					'showSynchronizationLink' => 1,
 					'showPossibleLocalizationRecords' => 1,
 					'showAllLocalizationLink' => 1
-				),
-			),
-		),
-		'map_center' => array(
+				],
+			],
+		],
+		'map_center' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.map_center',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 20,
 				'eval' => 'trim'
-			)
-		),
-		'map_zoom' => array(
+			]
+		],
+		'map_zoom' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.map_zoom',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 3,
 				'eval' => 'int'
-			)
-		),
-		'map_type' => array(
+			]
+		],
+		'map_type' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.map_type',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 20,
 				'eval' => 'trim'
-			)
-		),
-		'infomail' => array(
+			]
+		],
+		'infomail' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.infomail',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
-		'notification' => array(
+			]
+		],
+		'notification' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.notification',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
+			]
+		],
 		'public' => array(
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.public',
@@ -334,92 +334,92 @@ return array(
 				'default' => 0
 			)
 		),
-		'closed' => array(
+		'closed' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.closed',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
-		'series' => array(
+			]
+		],
+		'series' => [
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series',
-			'config' => array(
+			'config' => [
 				'type' => 'check',
 				'default' => 0
-			)
-		),
-		'series_start' => array(
+			]
+		],
+		'series_start' => [
 			'displayCond' => 'FIELD:series:>:0',
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series_start',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'dbType' => 'date',
 				'renderType' => 'inputDateTime',
 				'eval' => 'date,int',
-			),
-		),
-		'series_end' => array(
+			],
+		],
+		'series_end' => [
 			'displayCond' => 'FIELD:series:>:0',
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series_end',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'dbType' => 'date',
 				'renderType' => 'inputDateTime',
 				'eval' => 'date,int',
-			),
-		),
-		'series_period' => array(
+			],
+		],
+		'series_period' => [
 			'displayCond' => 'FIELD:series:>:0',
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series_period',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
-				'items' => array(
-					array('wöchentlich', 0),
-					array('monatlich', 1),
-				),
-			),
-		),
-		'series_number' => array(
+				'items' => [
+					['wöchentlich', 0],
+					['monatlich', 1],
+				],
+			],
+		],
+		'series_number' => [
 			'displayCond' => 'FIELD:series:>:0',
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series_number',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
-				'items' => array(
-					array('ersten', 0),
-					array('zweiten', 1),
-					array('dritten', 2),
-					array('vierten', 3),
-					array('letzten', 4),
-				),
-			),
-		),
-		'series_weekday' => array(
+				'items' => [
+					['ersten', 0],
+					['zweiten', 1],
+					['dritten', 2],
+					['vierten', 3],
+					['letzten', 4],
+				],
+			],
+		],
+		'series_weekday' => [
 			'displayCond' => 'FIELD:series:>:0',
 			'exclude' => 1,
 			'label' => 'LLL:EXT:trainingsplatz/Resources/Private/Language/locallang_db.xlf:tx_trainingsplatz_domain_model_training.series_weekday',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
-				'items' => array(
-					array('Montag', 0),
-					array('Dienstag', 1),
-					array('Mittwoch', 2),
-					array('Donnerstag', 3),
-					array('Freitag', 4),
-					array('Samstag', 5),
-					array('Sonntag', 6),
-				),
-			),
-		),
+				'items' => [
+					['Montag', 0],
+					['Dienstag', 1],
+					['Mittwoch', 2],
+					['Donnerstag', 3],
+					['Freitag', 4],
+					['Samstag', 5],
+					['Sonntag', 6],
+				],
+			],
+		],
 
-	),
-);
+	],
+];
 ?>
