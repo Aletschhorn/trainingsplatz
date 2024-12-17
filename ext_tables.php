b53f4c97fe0886367275e1671ecf440f6182bccb
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2024-12-17 21:54:53 +0100

    Remove unused TCA field descriptions

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,6 @@ defined('TYPO3_MODE') || die();
 
 foreach (['training', 'intensity', 'sport', 'map', 'answer', 'infomail', 'motivation', 'template'] as $table) {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_trainingsplatz_domain_model_' . $table);
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_trainingsplatz_domain_model_' . $table, 
-	'EXT:trainingsplatz/Resources/Private/Language/locallang_csh_tx_trainingsplatz_domain_model_' . $table . '.xlf');
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('trainingsplatz', 'Configuration/TypoScript', 'Trainingsplatz');
