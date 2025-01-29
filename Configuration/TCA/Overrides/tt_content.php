1f16440a9eff926c1a6e39dfab020f7804f39137
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2025-01-29 15:05:33 +0100

    Harmonization and corrections

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,74 +3,75 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-call_user_func(
-    function () {
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'TrainingList',
-			'Training List'
-		);
+defined('TYPO3') or die();
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'TrainingEvaluation',
-			'Training Evaluation'
-		);
+(static function (): void {
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'TrainingList',
+		'Training List'
+	);
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'TrainingParticipation',
-			'Training Participation'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'TrainingEvaluation',
+		'Training Evaluation'
+	);
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'TrainingParticipationUser',
-			'Training Participation User View'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'TrainingParticipation',
+		'Training Participation'
+	);
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'CompetitionRanking',
-			'Competition Ranking'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'TrainingParticipationUser',
+		'Training Participation User View'
+	);
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'CompetitionUser',
-			'Competition User View'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'CompetitionRanking',
+		'Competition Ranking'
+	);
 
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'Admin',
-			'Trainingsplatz Admin Tools'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'CompetitionUser',
+		'Competition User View'
+	);
+
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'Admin',
+		'Trainingsplatz Admin Tools'
+	);
 		
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'Infomail',
-			'Training Infomails'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'Infomail',
+		'Training Infomails'
+	);
 		
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'Birthday',
-			'Member Birthdays'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'Birthday',
+		'Member Birthdays'
+	);
 		
-		ExtensionUtility::registerPlugin(
-			'trainingsplatz',
-			'Messaging',
-			'Member Messaging'
-		);
+	ExtensionUtility::registerPlugin(
+		'trainingsplatz',
+		'Messaging',
+		'Member Messaging'
+	);
 		
-		// register flexforms
-		foreach (['traininglist', 'competitionranking', 'birthday'] as $plugin) {
-			$pluginSignature = 'trainingsplatz_'.$plugin;
-			$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-			ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:trainingsplatz/Configuration/FlexForms/flexform_'.$plugin.'.xml'); 
-		}
-    }
-);
+	// register flexforms
+	foreach (['traininglist', 'competitionranking', 'birthday'] as $plugin) {
+		$pluginSignature = 'trainingsplatz_'.$plugin;
+		$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+		ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:trainingsplatz/Configuration/FlexForms/flexform_'.$plugin.'.xml'); 
+	}
+
+})();
 ?>
