eb002f95823bd41801cb5987efec21420ededc5b
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2025-01-29 15:03:16 +0100

    Delete ext_tables.php

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-foreach (['training', 'intensity', 'sport', 'map', 'answer', 'infomail', 'motivation', 'template'] as $table) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_trainingsplatz_domain_model_' . $table);
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('trainingsplatz', 'Configuration/TypoScript', 'Trainingsplatz');
-
