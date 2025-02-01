0930f0c315395bd4c2e4e98679d291cafb4b48ae
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2025-02-01 14:39:04 +0100

    Add 5th parameter to configurePlugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,93 +1,89 @@
 <?php
 declare(strict_types=1);
 
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use DW\Trainingsplatz\Controller\TrainingController;
 use DW\Trainingsplatz\Controller\InfomailController;
 use DW\Trainingsplatz\Controller\UserController;
 
 defined('TYPO3') or die();
 
-(static function() {
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'TrainingList',
-		[TrainingController::class => 'list, show, new, create, edit, update, delete, cancel, activate, addAnswer, editAnswer, modifyAnswer, cancelAnswer, reactivateAnswer, deleteAnswer, message, messageSend, cancelRequestAnswer, cancelPublicAnswer'],
-		[TrainingController::class => 'new, create, edit, update, delete, cancel, activate, addAnswer, editAnswer, modifyAnswer, cancelAnswer, reactivateAnswer, deleteAnswer, message, messageSend, cancelRequestAnswer, cancelPublicAnswer']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'TrainingEvaluation',
-		[TrainingController::class => 'evaluate, close, billing, discount, finalize'],
-		[TrainingController::class => 'close, billing, discount, finalize']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'TrainingParticipation',
-		[TrainingController::class => 'participation, userParticipation'],
-		[TrainingController::class => 'userParticipation']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'TrainingParticipationUser',
-		[TrainingController::class => 'userParticipation'],
-		[TrainingController::class => 'userParticipation']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'CompetitionRanking',
-		[TrainingController::class => 'ranking'],
-		[TrainingController::class => '']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'CompetitionUser',
-		[TrainingController::class => 'userCompetition'],
-		[TrainingController::class => '']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'Admin',
-		[TrainingController::class => 'analysis, ranking, reports, single'],
-		[TrainingController::class => '']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'Infomail',
-		[InfomailController::class => 'list, show, review, copy, send, delete, deny, cancel'],
-		[InfomailController::class => 'create, copy, send, delete, deny, cancel']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'Birthday',
-		[UserController::class => 'birthday'],
-		[UserController::class => '']
-	);
-	
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Trainingsplatz',
-		'Messaging',
-		[UserController::class => 'message, messageSend'],
-		[UserController::class => 'message, messageSend']
-	);
-	
-	
-	// Overwrite object classes to extend femanager fields
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Femanager\Controller\NewController::class] = ['className' => \DW\Trainingsplatz\Controller\NewController::class];
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Femanager\Controller\EditController::class] = ['className' => \DW\Trainingsplatz\Controller\EditController::class];
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Femanager\Domain\Model\User::class] = ['className' => \DW\Trainingsplatz\Domain\Model\User::class];
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Femanager\Domain\Repository\UserRepository::class] = ['className' => \DW\Trainingsplatz\Domain\Repository\UserRepository::class];
-	
-	// Register type converter
-	if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 12) {
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\DW\Trainingsplatz\Property\TypeConverter\BitConverter::class);
-	}
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'TrainingList',
+	[TrainingController::class => 'list, show, new, create, edit, update, delete, cancel, activate, addAnswer, editAnswer, modifyAnswer, cancelAnswer, reactivateAnswer, deleteAnswer, message, messageSend, cancelRequestAnswer, cancelPublicAnswer'],
+	[TrainingController::class => 'new, create, edit, update, delete, cancel, activate, addAnswer, editAnswer, modifyAnswer, cancelAnswer, reactivateAnswer, deleteAnswer, message, messageSend, cancelRequestAnswer, cancelPublicAnswer'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
 
-})();
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'TrainingEvaluation',
+	[TrainingController::class => 'evaluate, close, billing, discount, finalize'],
+	[TrainingController::class => 'close, billing, discount, finalize'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'TrainingParticipation',
+	[TrainingController::class => 'participation, userParticipation'],
+	[TrainingController::class => 'userParticipation'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'TrainingParticipationUser',
+	[TrainingController::class => 'userParticipation'],
+	[TrainingController::class => 'userParticipation'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'CompetitionRanking',
+	[TrainingController::class => 'ranking'],
+	[TrainingController::class => ''],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'CompetitionUser',
+	[TrainingController::class => 'userCompetition'],
+	[TrainingController::class => ''],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'Admin',
+	[TrainingController::class => 'analysis, ranking, reports, single'],
+	[TrainingController::class => ''],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'Infomail',
+	[InfomailController::class => 'list, show, review, copy, send, delete, deny, cancel'],
+	[InfomailController::class => 'create, copy, send, delete, deny, cancel'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'Birthday',
+	[UserController::class => 'birthday'],
+	[UserController::class => ''],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionUtility::configurePlugin(
+	'Trainingsplatz',
+	'Messaging',
+	[UserController::class => 'message, messageSend'],
+	[UserController::class => 'message, messageSend'],
+	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
